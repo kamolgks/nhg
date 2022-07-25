6a5db8015da9628164031eb89ad29c26980316b3
Kamol <101630621+kamolgks@users.noreply.github.com>
2022-07-25 19:45:13 +0500

    Update text.js

--- text.js
+++ text.js
@@ -1,32 +1,4 @@
-$(function () { 
-    var title = document.title 
-        , animSeq = ["/", "/", "\\", "|", "/"] 
-        , animIndex = 0 
-        , titleIndex = 0; 
- 
-    function doInverseSpinZeroPitch() { 
-        var loadTitle = title.substring(0, titleIndex); 
-        if (titleIndex > title.length) { 
-            animIndex = 0; 
-            titleIndex = 0 
-        } 
-        if (animIndex > 3) { 
-            titleIndex++; 
-            animIndex = 0 
-        } 
-        document.title = loadTitle + animSeq[animIndex]; 
-        animIndex++ 
-    } 
-    window.setInterval(doInverseSpinZeroPitch, 150); 
-}); 
-$(function () { 
-    $(".typed").typed({ 
-        strings: ["Azizbek Xamidov"], 
-        typeSpeed: 40, 
-        backSpeed: 0, 
-        cursorChar: ['❚'], 
-        smartBackspace: true, 
-        fadeOut: true, 
-        loop: true, 
-    }); 
-});
+$(function(){var title=document.title,animSeq=["|"],animIndex=0,titleIndex=0;function doInverseSpinZeroPitch(){var loadTitle=title.substring(0,titleIndex);if(titleIndex>title.length){animIndex=0;titleIndex=0}
+if(animIndex>1){titleIndex++;animIndex=0}
+document.title=loadTitle+animSeq[0];animIndex++}
+window.setInterval(doInverseSpinZeroPitch,150);});$(function(){$(".typed").typed({strings:['Me',"Kamol","Coder","good conversationalist ","Python nedoпрораммист","Learn Python / Html"],typeSpeed:100,loop:true,cursorChar:"|",});});
