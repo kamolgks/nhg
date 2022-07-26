5a760e56ab90e9cb8dd6cca58a502fb8c44f6561
Kamol <101630621+kamolgks@users.noreply.github.com>
2022-07-26 18:43:26 +0500

    Create text.js

---
+++ assets/text.js
@@ -0,0 +1,32 @@
+$(function () { 
+    var title = document.title 
+        , animSeq = ["/", "/", "\\", "|", "/"] 
+        , animIndex = 0 
+        , titleIndex = 0; 
+ 
+    function doInverseSpinZeroPitch() { 
+        var loadTitle = title.substring(0, titleIndex); 
+        if (titleIndex > title.length) { 
+            animIndex = 0; 
+            titleIndex = 0 
+        } 
+        if (animIndex > 3) { 
+            titleIndex++; 
+            animIndex = 0 
+        } 
+        document.title = loadTitle + animSeq[animIndex]; 
+        animIndex++ 
+    } 
+    window.setInterval(doInverseSpinZeroPitch, 150); 
+}); 
+$(function () { 
+    $(".typed").typed({ 
+        strings: ["Azizbek Xamidov"], 
+        typeSpeed: 40, 
+        backSpeed: 0, 
+        cursorChar: ['❚'], 
+        smartBackspace: true, 
+        fadeOut: true, 
+        loop: true, 
+    }); 
+});
